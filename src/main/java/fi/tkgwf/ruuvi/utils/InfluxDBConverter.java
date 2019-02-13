c64e3b2d282895dc29f8be3ffe4edd0f2fab598d
Ville Saalo <ville.saalo@iki.fi>
2019-02-13 23:17:40 +0200

    Refactor InfluxDBConverter to use the new getters of RuuviMeasurement

--- src/main/java/fi/tkgwf/ruuvi/utils/InfluxDBConverter.java
+++ src/main/java/fi/tkgwf/ruuvi/utils/InfluxDBConverter.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.influxdb.dto.BatchPoints;
 import org.influxdb.dto.Point;
+import java.util.function.Function;
 
 public class InfluxDBConverter {
 
@@ -26,31 +27,35 @@ public class InfluxDBConverter {
         if (measurement.time != null) {
             p.time(measurement.time, TimeUnit.MILLISECONDS);
         }
-        addValueIfNotNull(p, "temperature", measurement.temperature);
-        addValueIfNotNull(p, "humidity", measurement.humidity);
-        addValueIfNotNull(p, "pressure", measurement.pressure);
-        addValueIfNotNull(p, "accelerationX", measurement.accelerationX);
-        addValueIfNotNull(p, "accelerationY", measurement.accelerationY);
-        addValueIfNotNull(p, "accelerationZ", measurement.accelerationZ);
-        addValueIfNotNull(p, "batteryVoltage", measurement.batteryVoltage);
-        addValueIfNotNull(p, "txPower", measurement.txPower);
-        addValueIfNotNull(p, "movementCounter", measurement.movementCounter);
-        addValueIfNotNull(p, "measurementSequenceNumber", measurement.measurementSequenceNumber);
-        addValueIfNotNull(p, "rssi", measurement.rssi);
+        addValueIfNotNull(p, "temperature", measurement, RuuviMeasurement::getTemperature);
+        addValueIfNotNull(p, "humidity", measurement, RuuviMeasurement::getHumidity);
+        addValueIfNotNull(p, "pressure", measurement, RuuviMeasurement::getPressure);
+        addValueIfNotNull(p, "accelerationX", measurement, RuuviMeasurement::getAccelerationX);
+        addValueIfNotNull(p, "accelerationY", measurement, RuuviMeasurement::getAccelerationY);
+        addValueIfNotNull(p, "accelerationZ", measurement, RuuviMeasurement::getAccelerationZ);
+        addValueIfNotNull(p, "batteryVoltage", measurement, RuuviMeasurement::getBatteryVoltage);
+        addValueIfNotNull(p, "txPower", measurement, RuuviMeasurement::getTxPower);
+        addValueIfNotNull(p, "movementCounter", measurement, RuuviMeasurement::getMovementCounter);
+        addValueIfNotNull(p, "measurementSequenceNumber", measurement, RuuviMeasurement::getMeasurementSequenceNumber);
+        addValueIfNotNull(p, "rssi", measurement, RuuviMeasurement::getRssi);
         if (extended) {
-            addValueIfNotNull(p, "accelerationTotal", measurement.accelerationTotal);
-            addValueIfNotNull(p, "absoluteHumidity", measurement.absoluteHumidity);
-            addValueIfNotNull(p, "dewPoint", measurement.dewPoint);
-            addValueIfNotNull(p, "equilibriumVaporPressure", measurement.equilibriumVaporPressure);
-            addValueIfNotNull(p, "airDensity", measurement.airDensity);
-            addValueIfNotNull(p, "accelerationAngleFromX", measurement.accelerationAngleFromX);
-            addValueIfNotNull(p, "accelerationAngleFromY", measurement.accelerationAngleFromY);
-            addValueIfNotNull(p, "accelerationAngleFromZ", measurement.accelerationAngleFromZ);
+            addValueIfNotNull(p, "accelerationTotal", measurement, RuuviMeasurement::getAccelerationTotal);
+            addValueIfNotNull(p, "absoluteHumidity", measurement, RuuviMeasurement::getAbsoluteHumidity);
+            addValueIfNotNull(p, "dewPoint", measurement, RuuviMeasurement::getDewPoint);
+            addValueIfNotNull(p, "equilibriumVaporPressure", measurement, RuuviMeasurement::getEquilibriumVaporPressure);
+            addValueIfNotNull(p, "airDensity", measurement, RuuviMeasurement::getAirDensity);
+            addValueIfNotNull(p, "accelerationAngleFromX", measurement, RuuviMeasurement::getAccelerationAngleFromX);
+            addValueIfNotNull(p, "accelerationAngleFromY", measurement, RuuviMeasurement::getAccelerationAngleFromY);
+            addValueIfNotNull(p, "accelerationAngleFromZ", measurement, RuuviMeasurement::getAccelerationAngleFromZ);
         }
         return p.build();
     }
 
-    private static void addValueIfNotNull(Point.Builder point, String name, Number value) {
+    private static void addValueIfNotNull(Point.Builder point,
+                                          String name,
+                                          RuuviMeasurement measurement,
+                                          Function<RuuviMeasurement, ? extends Number> getter) {
+        final Number value = getter.apply(measurement);
         if (value != null) {
             point.addField(name, value);
         }
