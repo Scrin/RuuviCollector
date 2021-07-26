cdff5ff5b4152cb8a45adb95d3d2815f94783fae
Scrin <Microx256@gmail.com>
2021-07-26 18:31:36 +0300

    Fix typos and metric types in prometheus exporter, make prometheus exporter tolerate missing fields

--- src/main/java/fi/tkgwf/ruuvi/db/PrometheusExporter.java
+++ src/main/java/fi/tkgwf/ruuvi/db/PrometheusExporter.java
@@ -39,15 +39,15 @@ public final class PrometheusExporter implements DBConnection {
     private final Gauge accelerationZ = buildGauge("acceleration_z", "Acceleration of Z axis in G", Gauge.build());
     private final Gauge batteryVoltage = buildGauge("battery_voltage", "Battery voltage in Volts", Gauge.build());
     private final Gauge txPower = buildGauge("tx_power", "TX power in dBm", Gauge.build());
-    private final Counter movementCounter = buildGauge("movement_counter", "Movement counter (incremented by interrupts from the accelerometer)", Counter.build());
-    private final Counter measurementSequenceNumber = buildGauge("measurement_sequence_number",
-        "Measurement sequence number (incremented every time a new measurement is made). Useful for measurement de-duplication.", Counter.build());
+    private final Gauge movementCounter = buildGauge("movement_counter", "Movement counter (incremented by interrupts from the accelerometer)", Gauge.build());
+    private final Gauge measurementSequenceNumber = buildGauge("measurement_sequence_number",
+        "Measurement sequence number (incremented every time a new measurement is made). Useful for measurement de-duplication.", Gauge.build());
 
     // "enhanced" ones
     private final Gauge accelerationTotal = buildGauge("acceleration_total", "Total acceleration", Gauge.build());
     private final Gauge accelerationAngleFromX = buildGauge("acceleration_angle_from_x", "The angle between the acceleration vector and X axis", Gauge.build());
     private final Gauge accelerationAngleFromY = buildGauge("acceleration_angle_from_y", "The angle between the acceleration vector and Y axis", Gauge.build());
-    private final Gauge accelerationAngleFromZ = buildGauge("acceleration_angle_fromz", "The angle between the acceleration vector and Z axis", Gauge.build());
+    private final Gauge accelerationAngleFromZ = buildGauge("acceleration_angle_from_z", "The angle between the acceleration vector and Z axis", Gauge.build());
     private final Gauge absoluteHumidity = buildGauge("absolute_humidity", "Absolute humidity in g/m^3", Gauge.build());
     private final Gauge dewPoint = buildGauge("dew_point", "Dew point in Celsius", Gauge.build());
     private final Gauge equilibriumVaporPressure = buildGauge("equilibrium_vapor_pressure", "Vapor pressure of water", Gauge.build());
@@ -78,7 +78,6 @@ public final class PrometheusExporter implements DBConnection {
 
     @Override
     public void save(EnhancedRuuviMeasurement measurement) {
-        LOG.debug("New measurement: " + measurement);
 
         String mac = measurement.getMac();
         String name = Optional.ofNullable(measurement.getName()).orElse(mac);
@@ -87,40 +86,35 @@ public final class PrometheusExporter implements DBConnection {
 
         prometheusExportedCount.labels(mac, name, dataFormat).inc();
 
-        rssi.labels(mac, name, dataFormat).set(measurement.getRssi());
-        temperature.labels(mac, name, dataFormat).set(measurement.getTemperature());
-        humidity.labels(mac, name, dataFormat).set(measurement.getHumidity());
-        pressure.labels(mac, name, dataFormat).set(measurement.getPressure());
-        accelerationX.labels(mac, name, dataFormat).set(measurement.getAccelerationX());
-        accelerationY.labels(mac, name, dataFormat).set(measurement.getAccelerationY());
-        accelerationZ.labels(mac, name, dataFormat).set(measurement.getAccelerationZ());
-        batteryVoltage.labels(mac, name, dataFormat).set(measurement.getBatteryVoltage());
-        txPower.labels(mac, name, dataFormat).set(measurement.getTxPower());
+        setValue(rssi.labels(mac, name, dataFormat), measurement.getRssi());
+        setValue(temperature.labels(mac, name, dataFormat), measurement.getTemperature());
+        setValue(humidity.labels(mac, name, dataFormat), measurement.getHumidity());
+        setValue(pressure.labels(mac, name, dataFormat), measurement.getPressure());
+        setValue(accelerationX.labels(mac, name, dataFormat), measurement.getAccelerationX());
+        setValue(accelerationY.labels(mac, name, dataFormat), measurement.getAccelerationY());
+        setValue(accelerationZ.labels(mac, name, dataFormat), measurement.getAccelerationZ());
+        setValue(batteryVoltage.labels(mac, name, dataFormat), measurement.getBatteryVoltage());
+        setValue(txPower.labels(mac, name, dataFormat), measurement.getTxPower());
 
-        updateCounter(measurementSequenceNumber.labels(mac, name, dataFormat),
-            measurement.getMeasurementSequenceNumber());
-        updateCounter(movementCounter.labels(mac, name, dataFormat),
-            measurement.getMovementCounter());
+        setValue(measurementSequenceNumber.labels(mac, name, dataFormat), measurement.getMeasurementSequenceNumber());
+        setValue(movementCounter.labels(mac, name, dataFormat), measurement.getMovementCounter());
 
-        accelerationTotal.labels(mac, name, dataFormat).set(measurement.getAccelerationTotal());
-        accelerationAngleFromX.labels(mac, name, dataFormat).set(measurement.getAccelerationAngleFromX());
-        accelerationAngleFromY.labels(mac, name, dataFormat).set(measurement.getAccelerationAngleFromY());
-        accelerationAngleFromZ.labels(mac, name, dataFormat).inc(measurement.getAccelerationAngleFromZ());
-        absoluteHumidity.labels(mac, name, dataFormat).set(measurement.getAbsoluteHumidity());
-        dewPoint.labels(mac, name, dataFormat).set(measurement.getDewPoint());
-        equilibriumVaporPressure.labels(mac, name, dataFormat).set(measurement.getEquilibriumVaporPressure());
-        airDensity.labels(mac, name, dataFormat).set(measurement.getAirDensity());
+        setValue(accelerationTotal.labels(mac, name, dataFormat), measurement.getAccelerationTotal());
+        setValue(accelerationAngleFromX.labels(mac, name, dataFormat), measurement.getAccelerationAngleFromX());
+        setValue(accelerationAngleFromY.labels(mac, name, dataFormat), measurement.getAccelerationAngleFromY());
+        setValue(accelerationAngleFromZ.labels(mac, name, dataFormat), measurement.getAccelerationAngleFromZ());
+        setValue(absoluteHumidity.labels(mac, name, dataFormat), measurement.getAbsoluteHumidity());
+        setValue(dewPoint.labels(mac, name, dataFormat), measurement.getDewPoint());
+        setValue(equilibriumVaporPressure.labels(mac, name, dataFormat), measurement.getEquilibriumVaporPressure());
+        setValue(airDensity.labels(mac, name, dataFormat), measurement.getAirDensity());
 
         lastUpdate.labels(mac, name, dataFormat).setToCurrentTime();
     }
 
-    /**
-     * Updates a {@link Counter} to reflect a value changed in an upstream value that claims to be a counter. This is
-     * like calling {@code set} on a Counter that only has {@code inc}. If the new value is less than the current value
-     * of the counter, the update is dropped.
-     */
-    private static void updateCounter(Counter.Child counter, Integer newValue) {
-        counter.inc(Math.max(0, newValue - (int) counter.get()));
+    private static void setValue(Gauge.Child gauge, Number value) {
+        if (value != null) {
+            gauge.set(value.doubleValue());
+        }
     }
 
     @Override
